a07363338478c142d3b4d8239efd3df7eab80723
Millena <lmists13@gmail.com>
2021-10-04 01:25:08 -0300

    Addition of functions phi

--- ep01/iteracaoLinear.c
+++ ep01/iteracaoLinear.c
@@ -5,28 +5,38 @@
 #include <string.h>
 #include <matheval.h>
 
+double phiNr(double x, char *f) {
+    void *g = evaluator_create (f);  
+    void *dg = evaluator_derivative_x (f);
+    assert (g);
+    assert (dg);
+    return (x - (evaluator_evaluate_x(g, x)) / evaluator_evaluate_x(dg, x)); // x_{i+1} = - (f(x_i)/f'(x_i))
+}
 
-double iteracao(double x0, double f(double x), double phi(double x),
-                double epsilon_x, int max_iter){
+double phiSecante(double x, double xOld , char *f) {
+    double resOld = function(xOld, f);
+    double res = function(x, f);
 
-    double x_new, x_old = x0;
-    int criterio1, criterio2, criterio3;
-    int iter = 1;
+    return (x - (res * (x - xOld)) / (res - resOld)); // x_{i+1} = x_i - f(x_i)*(x_i - x_{i-1}) / f(x_i) - f(x_{i-1})
+}
 
-    do {
-        x_old = x_new;
-        x_new = phi(x_old); // metodo linear normal;
+double function(double x, char *f) {
+    void *g = evaluator_create(f);
+    assert(g);
+    
+    return evaluator_evaluate_x(g, x);
+}
 
-        criterio1 = (fabs(x_old - x_new) < epsilon_x);
-        // criterio2 = (fabs(f(x_new)) < epsilon_f);
-        criterio3 = (iter == max_iter);
-        ++iter;
-    } while(!criterio1 && !criterio2);
+double functionDerivative(double x, char *f) {
+    void *g = evaluator_create (f);
+    assert(g);
+    *g = evaluator_derivative_x(g);
+    assert(g);
 
-    return x_new;
- }
+    return evaluator_evaluate_x(g, x);
+}
 
-double newtonRaphson(double x0, double f(double x), double epsilon_x, int max_iter){
+double newtonRaphson(double x0, char *f, double phi(double x), double epsilon_x, int max_iter){
 
     double x_new, x_old = x0;
     int criterio1, criterio2;
@@ -34,7 +44,8 @@ double newtonRaphson(double x0, double f(double x), double epsilon_x, int max_it
 
     do {
         x_old = x_new;
-        x_new = x_old - (phi(x_old)/derivative(x_old));                   // xi - ( f(xi)/f'(xi) )
+        x_new = phi(x_old);  //x_old - ((f(x_old))/derivada(x_old));                   // xi - ( f(xi)/f'(xi) )
+        
 
         criterio1 = (fabs(x_old - x_new) < epsilon_x);
         criterio2 = (i == max_iter);
@@ -46,22 +57,15 @@ double newtonRaphson(double x0, double f(double x), double epsilon_x, int max_it
 
 int main(int argc, char *argv[]){
   
-    char *function = argv[1];
-    double initialAprox = atof(argv[2]);
-    double epsilon = atof(argv[3]);
-    int max_iter = atoi(argv[4]);
+    char *string = argv[1];
+    // double initialAprox = atof(argv[2]);
+    // double epsilon = atof(argv[3]);
+    // int max_iter = atoi(argv[4]);
 
     // printf("F(x): %s, x0: %1.16e, Epsilon: %1.16e, Max iterations: %d\n", function, initialAprox, epsilon, max_iter);
 
-
-    // Testing libmatheval
-    void *f;
-    
-    f = evaluator_create (function);        // function: x^2-2
-    assert (f);
-
-    
-    printf ("f(0.1) = %g\n", evaluator_evaluate_x (f, 0.1));
+    printf ("f(0.1) = %g\n", function(0.1, string));
+    printf ("f'(0.1) = %g\n", functionDerivative(0.1, string));
     // iteracao(initialAprox, function, phi, epsilon, max_iter);
     // newtonRaphson(initialAprox, function, epsilon, max_iter);
 
