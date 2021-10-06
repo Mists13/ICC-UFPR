39d5ce8b32893bc79095185bee3f09bb2e8cc614
Millena <lmists13@gmail.com>
2021-10-06 09:07:55 -0300

    Ulp e criterio de parada

--- ep01/iteracaoLinear.c
+++ ep01/iteracaoLinear.c
@@ -9,25 +9,19 @@
 
 double fx(double x, char *f) {
     void *g = evaluator_create(f);
-    assert(g);
     
     return evaluator_evaluate_x(g, x);
 }
 
 double fxDerivative(double x, char *f) {
     void *g = evaluator_create (f);
-    // assert(g);
-    
     g = evaluator_derivative_x(g);
-    // assert(g);
-    
+
     return evaluator_evaluate_x(g, x);
 }
 
 double phiNewton(double x, char *f) {
     double result = (x - (fx(x, f)) / fxDerivative(x, f)); // x_{i+1} = x - ( f(x_i)/f'(x_i) )
-    // double resDeriv = fxDerivative(x, f);
-    // printf("PhiNr -> derivative result: = %lf\n", resDeriv );
     return result;
 }
 
@@ -35,21 +29,27 @@ double phiSecante(double x, double xOld , char *f) {
     double res = fx(x, f);
     double resOld = fx(xOld, f);
 
-    return (x - (res * (x - xOld)) / (res - resOld)); // x_{i+1} = x_i - ( f(x_i)*(x_i - x_{i-1}) / f(x_i) - f(x_{i-1}) )
+    return (x - ((res * x) / (res - resOld) - (res * xOld) / (res - resOld))); // x_{i+1} = x_i - ( f(x_i)*(x_i - x_{i-1}) / f(x_i) - f(x_{i-1}) )
 }
 
+long int ulp(double newton_x_new, double secante_x_new) {
+    long int secante_x_new_int, newton_x_new_int;
+   
+    memcpy(&newton_x_new_int, &newton_x_new, sizeof(newton_x_new));
+    memcpy(&secante_x_new_int, &secante_x_new, sizeof(newton_x_new));
+   
+    return abs(newton_x_new_int - secante_x_new_int -1);
+}
 
 void newtonRaphsonESecante(double x0, char *f, double phiNewton(double x, char *f), double epsilon_x, int max_iter) {
-
     double newton_x_new, newton_x, secante_x, secante_x_new, secante_x_old;
     double newton_crit, secante_crit;
-    double abs_error, relative_error;
+    double abs_error, relative_error, aux;
     int stop, stop_newton, stop_secante, i;
 
-    printf("i, newton_x, newton_crit, secante_x, secante_crit\n");
     newton_crit = secante_crit = fabs(fx(x0, f));
     // printa primeira aproximação - valor de input
-    printf("%d,%1.16e,%1.16e,%1.16e,%1.16e", 0, x0, newton_crit, x0, secante_crit);  
+    printf("%d, %1.16e, %1.16e, %1.16e, %1.16e, %1.16e, %1.16e, %d\n", 0, x0, newton_crit, x0, secante_crit, 0.0, 0.0, 0);  
 
     // o valor da primeira iteração para o metódo da secante é o resultado da primeira iteração do método newton
     secante_x = x0;
@@ -58,41 +58,49 @@ void newtonRaphsonESecante(double x0, char *f, double phiNewton(double x, char *
     stop_newton = stop_secante = (newton_crit < epsilon_x);
 
     // printa primeira iteração
-    printf("%d,%1.16e,%1.16e,%1.16e,%1.16e\n", 0, newton_x_new, newton_crit, secante_x_new, secante_crit);
+    printf("%d, %1.16e, %1.16e, %1.16e, %1.16e, %1.16e, %1.16e, %d\n", 1, newton_x_new, newton_crit, secante_x_new, secante_crit, 0.0, 0.0, 0);
 
-    i = 2;
-    while ((i != max_iter) && !(stop_newton && stop_secante))
-    {
+
+    for (i = 2; (i <= max_iter) && !(stop_newton && stop_secante); ++i){
         if (!stop_secante){
             secante_x_old = secante_x;
             secante_x = secante_x_new;
             secante_x_new = phiSecante(secante_x, secante_x_old, f);
             
-            secante_crit = fabs(fx(secante_x_new, f));
+
+            aux = fx(secante_x_new, f);
+            secante_crit = (aux != 0.0) ? fabs(1 - (fx(secante_x, f) / aux)) * 100 : 0;
             stop_secante = (secante_crit < epsilon_x);
         }
         
         if (!stop_newton) {
             newton_x = newton_x_new;    
             newton_x_new = phiNewton(newton_x, f); 
-
-            newton_crit = fabs(fx(newton_x_new, f));                   
+            
+            aux = fx(newton_x_new, f);
+            newton_crit = (aux != 0.0) ? fabs(1 - (fx(newton_x, f) / aux)) * 100 : 0;                 
             stop_newton = (newton_crit < epsilon_x);
         }
 
-        printf("%d,%1.16e,%1.16e,%1.16e,%1.16e\n", i, newton_x_new, newton_crit, secante_x_new, secante_crit); 
-        ++i;
+        abs_error = newton_x_new - secante_x_new;
+        relative_error = fabs(abs_error / newton_x_new);
+        
+        printf("%d, %1.16e, %1.16e, %1.16e, %1.16e, ", i, newton_x_new, newton_crit, secante_x_new, secante_crit);
+        printf("%1.16e, %1.16e, ", abs_error, relative_error);
+        printf("%ld\n", ulp(newton_x_new, secante_x_new));
     }
  }
 
 int main(int argc, char *argv[]){
-  
-    char *functionStr = argv[1];
-    double initialAprox = atof(argv[2]);
-    double epsilon = atof(argv[3]);
-    int max_iter = atoi(argv[4]);
+    char functionStr[50];
+    double initialAprox;
+    double epsilon;
+    int max_iter;
 
-    // printf("F(x): %s, x0: %1.16e, Epsilon: %1.16e, Max iterations: %d\n", functionStr, initialAprox, epsilon, max_iter);
+    scanf("%s", functionStr);
+    scanf("%lf", &initialAprox);
+    scanf("%lf", &epsilon);
+    scanf("%d", &max_iter);
 
     newtonRaphsonESecante(initialAprox, functionStr, phiNewton, epsilon, max_iter);
     return 0;
