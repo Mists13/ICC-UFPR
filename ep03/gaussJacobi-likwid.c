2edf1f5e662227e28c8beda6d44fedbfb4979c2b
Matheus Pacheco <matheus.pacheco.mico@outlook.com>
2021-11-29 23:34:22 -0300

    [#main] Add markers

--- ep03/gaussJacobi-likwid.c
+++ ep03/gaussJacobi-likwid.c
@@ -9,6 +9,8 @@
 
 int main(int argc, char ** argv) {
 
+  LIKWID_MARKER_INIT;
+
   if (argc < 2) {
     printf("uso: %s file_in\n\n", argv[0]);
     return 1;
@@ -22,15 +24,24 @@ int main(int argc, char ** argv) {
   FILE *f_out;
 
   f_out = fopen(markerName("out_gaussJacobi", n), "w");
+
+  LIKWID_MARKER_START("gauss-jacobi");
   gaussJacobi(f_in, f_out);
+  LIKWID_MARKER_STOP("gauss-jacobi");
+
   fclose(f_out);
   rewind(f_in);
 
   f_out = fopen(markerName("out_gaussJacobiOpt", n), "w");
+
+  LIKWID_MARKER_START("gauss-jacobi-opt");
   gaussJacobiOpt(f_in, f_out);
+  LIKWID_MARKER_STOP("gauss-jacobi-opt");
+  
   fclose(f_out);
   fclose(f_in);
 
+  LIKWID_MARKER_CLOSE;
   return 0;
 }
 
